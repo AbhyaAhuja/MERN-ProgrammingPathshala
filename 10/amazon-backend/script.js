3aaa0711da2711233c49cf1ed1f544f69dd6d5ea
Abhya Ahuja <109384700+AbhyaAhuja@users.noreply.github.com>
2024-04-24 22:07:21 +0530

    Update script.js

--- 10/amazon-backend/script.js
+++ 10/amazon-backend/script.js
@@ -5,11 +5,12 @@ const app = express();
 const test = require('./models/productsModel.js')
 const mongoose = require('mongoose');
 
+app.use(express.json()); //for body- for post to be able to access req body
 app.use('/api/products', productsRouter); //no dot here as it wasn't an obj
 
 const url = "mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.7mamtlu.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0"
 const databaseUser = 'AbhyaAhuja';
-const databasePassword = 'abhya123';
+const databasePassword  = 'abhya123';
 const databaseName = 'Amazon-Backend';
 
 let dbLink = url.replace("$_USERNAME_$", databaseUser);
@@ -18,7 +19,7 @@ dbLink = dbLink.replace("$_DB_NAME_$", databaseName);
 
 mongoose.connect(dbLink)
 .then(
-    ()=>console.log('----Database Connected---')
+    ()=>console.log('----Database Connected---') 
 );
 
 app.listen(1400,
