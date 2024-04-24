451faaa4dbd0919a4354282669ec1d4170a454a7
Abhya Ahuja <109384700+AbhyaAhuja@users.noreply.github.com>
2024-04-24 22:06:43 +0530

    Update productsRoutes.js

--- 10/amazon-backend/routes/productsRoutes.js
+++ 10/amazon-backend/routes/productsRoutes.js
@@ -5,6 +5,10 @@ const productRouter = express.Router();
 const productsController = require('../controllers/productsController.js')
 
 productRouter.route('/')
-.get(productsController.getAllProducts);
+.get(productsController.getAllProducts)
+.post(productsController.addProduct)
 
+productRouter.route('/:id')
+.put(productsController.replaceProduct)
+.delete(productsController.delProduct)
 module.exports = productRouter;
