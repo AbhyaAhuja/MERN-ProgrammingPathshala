a7d2c9aaf16c14da38fd2fdd5c7106487995096d
Abhya Ahuja <abhyaahuja@gmail.com>
2024-06-07 17:04:13 +0530

    Backend

--- KIET-MAJOR-BACKEND/routes/authRouter.js
+++ KIET-MAJOR-BACKEND/routes/authRouter.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const authRouter = express.Router();
-const {signup} = require("../controllers/authController.js")
+const {signup, login} = require("../controllers/authController.js")
 // const { generateImage } = require("../controllers/imageController.js");
 
 authRouter.route('/signup')
     .post(signup)
+
+authRouter.route('/login')
+    .post(login)
 
 module.exports = authRouter;
